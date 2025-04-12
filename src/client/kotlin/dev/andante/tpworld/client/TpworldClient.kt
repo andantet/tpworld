aeb43794501a471abe48f909ca1424ab119e4453
Daniel Orr <orrdaniel73@gmail.com>
2025-04-12 13:01:36 +0100

    init

---
+++ src/client/kotlin/dev/andante/tpworld/client/TpworldClient.kt
@@ -0,0 +1,9 @@
+package dev.andante.tpworld.client
+
+import net.fabricmc.api.ClientModInitializer
+
+class TpworldClient : ClientModInitializer {
+
+    override fun onInitializeClient() {
+    }
+}
---
+++ src/main/kotlin/dev/andante/tpworld/Tpworld.kt
@@ -0,0 +1,27 @@
+package dev.andante.tpworld
+
+import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback
+import net.minecraft.command.argument.DimensionArgumentType
+import net.minecraft.server.command.CommandManager.argument
+import net.minecraft.server.command.CommandManager.literal
+
+class Tpworld : ModInitializer {
+
+    override fun onInitialize() {
+        CommandRegistrationCallback.EVENT.register { dispatcher, registry, environment ->
+            dispatcher.register(
+                literal("tpworld")
+                    .then(
+                        argument("world", DimensionArgumentType.dimension())
+                            .executes { context ->
+                                val entity = context.source.entityOrThrow
+                                val dimension = DimensionArgumentType.getDimensionArgument(context, "world")
+                                entity.teleport(dimension, entity.x, entity.y, entity.z, setOf(), entity.yaw, entity.pitch, false)
+                                1
+                            }
+                    )
+            )
+        }
+    }
+}
